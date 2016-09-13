dbaa3598e2141a6c498e24b5f0bd9e5a9a3898af
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-09-13 16:08:41 +0300

    events_EventEmitter_1-2

--- ee.js
+++ ee.js
@@ -6,15 +6,4 @@ var EventEmitter = require('events').EventEmitter;
 
 var server = new EventEmitter;
 
-server.on('request', function(request) {
-    request.approved = true;
-});
-
-server.on('request', function(request) {
-    console.log(request);
-});
-
-
-server.emit('request', {from: "Client"});
-
-server.emit('request', {from: "Another Client"});
+server.emit('error');
