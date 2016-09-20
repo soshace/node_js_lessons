e3a2320b0a4d0b389f6103393e7d3bf1fd1a2fb1
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-09-20 15:33:29 +0300

    node_server_1-3

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ server.listen(1337, '127.0.0.1');
 
 var counter = 0;
 
+var emit = server.emit;
+server.emit = function(event/*, arg1, arg2,... */) {
+    console.log(event);
+    return emit.apply(this, arguments);
+};
 server.on('request', function(req, res) {
     res.end("Hello, world!"+ ++counter);
 });
