53776e7e58119ed34e4f799cf70719c3b5c0509c
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-09-20 16:27:57 +0300

    node_echo_server_1-1

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
+// http://127.0.0.1:3000/echo?message=Hello -> Hello
+
 var http = require('http');
 
-var server = new http.Server();
-
-server.listen(1337, '127.0.0.1');
-
-var counter = 0;
-
-var emit = server.emit;
-server.emit = function(event/*, arg1, arg2,... */) {
-    console.log(event);
-    return emit.apply(this, arguments);
-};
-server.on('request', function(req, res) {
-    res.end("Hello, world!"+ ++counter);
+var server = new http.Server(function(req, res) {
+    console.log( req.method, req.url);
 });
+
+server.listen(3000, '127.0.0.1');
