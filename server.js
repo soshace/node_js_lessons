c9d0b377d9221b88567e353ce7431af510454056
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-09-20 16:40:32 +0300

    node_echo_server_1-3

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ var url = require('url');
 
 var server = new http.Server(function(req, res) {
 
-    var urlParsed = url.parse(req.url, true);
+    console.log(req.headers );
 
-    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
-        res.end( urlParsed.query.message );
-    } else {
-        res.statusCode = 404; // Not Found
-        res.end("Page not found");
-    }
+
+//     var urlParsed = url.parse(req.url, true);
+//
+//     if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+//         res.end( urlParsed.query.message );
+//     } else {
+//         res.statusCode = 404; // Not Found
+//         res.end("Page not found");
+//     }
 });
 
 server.listen(3000, '127.0.0.1');
