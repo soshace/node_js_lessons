f66d71968e0862ab1457d1b51a5965eb6590e1b5
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-09-21 11:40:59 +0300

    development_supervisor_1-1

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var http = require('http');
 var url = require('url');
 
-var server = new http.Server(function(req, res) {
+var server = http.createServer();
 
+server.on('request', function(req, res) {
     var urlParsed = url.parse(req.url, true);
 
-    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
-        res.setHeader('Cache-control', 'no-cache');
-        res.end( urlParsed.query.message );
-    } else {
-        res.statusCode = 404; // Not Found
-        res.end("Page not found");
+    if (req.method == 'GET' && urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        res.end(urlParsed.query.message );
+        return;
     }
+
+    res.statusCode = 404;
+    res.end('Not Found');
 });
 
-server.listen(3000, '127.0.0.1');
+server.listen(1337);
