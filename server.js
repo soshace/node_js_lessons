a2861ddf824f464ee4dda3168121718ff8df6416
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-09-30 17:25:39 +0300

    async_1-3

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ http.createServer(function(req, res) {
 
     if (req.url == '/') {
          fs.readFile('index.html', function (err, info) { // callback
-
-            res.end('');
+             if (err) throw err;
+             res.end('');
         });
 
     }
