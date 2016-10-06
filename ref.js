ca3e56f52f125d93f5160a5da944621e7d46c984
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-10-06 17:35:57 +0300

    ref/unref_1-5

--- ref.js
+++ ref.js
@@ -2,16 +2,15 @@ var http = require('http');
 
 var server = new http.Server(function(req, res) {
 
-    /* request processing */
 
 }).listen(3000);
 
 setTimeout(function() {
-    server.close(function(){
-    clearInterval(timer);
-  });
+    server.close();
 }, 2500);
 
 var timer = setInterval(function() {
     console.log(process.memoryUsage());
 }, 1000);
+
+timer.unref();
