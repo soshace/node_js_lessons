b33ff1f4c3af0a31950dfb793d474437348592ce
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-10-18 16:27:55 +0300

    files_fs_1-5

--- read.js
+++ read.js
@@ -1,13 +1,6 @@
 var fs = require('fs');
 
-fs.readFile("blablabla", {encoding: 'utf-8'}, function(err, data) {
-    if (err) {
-        if (err.code == 'ENOENT') {
-            console.error(err.message);
-        } else {
-            console.error(err);
-        }
-    } else {
-        console.log(data);
-    }
+fs.stat(__filename, function(err, stats) {
+    console.log(stats.isFile());
+    console.log(stats);
 });
