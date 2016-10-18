bbb9b895926e0c1bc17fcef02e5c0544f0315b5a
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-10-18 16:42:06 +0300

    files_fs_1-6

--- read.js
+++ read.js
@@ -1,6 +1,14 @@
 var fs = require('fs');
 
-fs.stat(__filename, function(err, stats) {
-    console.log(stats.isFile());
-    console.log(stats);
+fs.writeFile("file.tmp", "data", function(err) {
+    if (err) throw err;
+
+    fs.rename("file.tmp", "new.tmp", function(err) {
+        if (err) throw err;
+
+        fs.unlink("new.tmp", function(err) {
+            if (err) throw err;
+        });
+    });
+
 });
