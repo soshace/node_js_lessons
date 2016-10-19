c339ec38b094c4ed65cae16a4535a3c276ff2a18
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-10-19 16:18:21 +0300

    data_stream_1-2

--- fs.js
+++ fs.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 // fs.ReadStream nherits from stream.Readable
-var stream = new fs.ReadStream(__filename);
+var stream = new fs.ReadStream(__filename, {encoding: 'utf-8'});
 
 stream.on('readable', function() {
     var data = stream.read();
@@ -11,3 +11,4 @@ stream.on('readable', function() {
 stream.on('end', function() {
     console.log("THE END");
 });
+
