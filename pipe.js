04ac74a7aa8c46a864056a752ad210d404f782e5
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-10-24 17:12:45 +0300

    writable_stream_1-5

--- pipe.js
+++ pipe.js
@@ -16,4 +16,21 @@ function sendFile(file, res) {
 
     file.pipe(res);
 
+    file.on('error', function(err) {
+        res.statusCode = 500;
+        res.end("Server Error");
+        console.error(err);
+    });
+
+    file
+        .on('open',function() {
+            console.log("open");
+        })
+        .on('close', function() {
+            console.log("close");
+        });
+
+    res.on('close', function() {
+        file.destroy();
+    });
 }
