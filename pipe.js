d07cf137038ebe7c52f4f2cb55dea4d391de78ee
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-10-24 15:46:43 +0300

    writable_stream_1-2

--- pipe.js
+++ pipe.js
@@ -6,15 +6,19 @@ new http.Server(function(req, res) {
 
     if (req.url == '/big.html') {
 
-        fs.readFile('big.html', function(err, content) {
-            if (err) {
-                res.statusCode = 500;
-                res.end('Server error');
-            } else {
-                res.setHeader("Content-type", "text/html; charset=utf-8");
-                res.end(content);
-            }
-        });
+        var file = new fs.ReadStream('big.html');
+        sendFile(file, res);
 
     }
 }).listen(3000);
+
+function sendFile(file, res) {
+
+    file.on('readable', write);
+
+    function write() {
+        var fileContent = file.read();
+
+        res.write(fileContent);
+    }
+}
