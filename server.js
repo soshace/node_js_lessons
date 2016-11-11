2404523ada9d950885eb8cc8383cbe0ede63897a
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-11 17:16:52 +0300

    domain_1-1

--- server.js
+++ server.js
@@ -1,68 +1,25 @@
 var http = require('http');
 var fs = require('fs');
-var chat = require('./chat');
 
+function handler(req, res) {
+    if (req.url == '/') {
 
-http.createServer(function(req, res) {
+        fs.readFile('index.html', function(err, content) {
+            if (err) {
+                console.error(err);
+                res.statusCode = 500;
+                res.end('There is an error on the server');
+                return
+            }
+            res.end(content);
+        });
 
-    switch (req.url) {
-        case '/':
-            sendFile("index.html", res);
-            break;
-
-        case '/subscribe':
-            chat.subscribe(req, res);
-            break;
-
-        case '/publish':
-            var body = '';
-
-            req
-                .on('readable', function() {
-                    var chunk = req.read();
-                    if (chunk !== null) {
-                        body += chunk;
-                    }
-                    if (body.length > 1000) {
-                        res.statusCode = 413;
-                        res.end("Your message is too big for my little chat");
-                    }
-                })
-                .on('end', function() {
-                    if (res.statusCode === 413) return;
-
-                    try {
-                        body = JSON.parse(body);
-                    } catch (e) {
-                        res.statusCode = 400;
-                        res.end("Bad Request");
-                        return;
-                    }
-                    chat.publish(body.message);
-                    res.end("ok");
-                });
-
-            break;
-
-
-        default:
-            res.statusCode = 404;
-            res.end("Not found");
+    } else {
+        res.statusCode = 404;
+        res.end("Not Found");
     }
 
-
-}).listen(3000);
-
-
-function sendFile(fileName, res) {
-    var fileStream = fs.createReadStream(fileName);
-    fileStream
-        .on('error', function () {
-            res.statusCode = 500;
-            res.end("Server error");
-        })
-        .pipe(res)
-        .on('close', function () {
-            fileStream.destroy();
-        });
 }
+
+var server = new http.createServer(handler);
+server.listen(3000);
