4b0b7fa636e678bb670553741c0501465af53880
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-15 16:44:48 +0300

    domain_1-11

---
+++ server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+function handler(req, res) {
+    if (req.url == '/') {
+
+        fs.readFile('no-such-file', function(err, content) {
+
+            if (err) throw err; // JSON.parse("invalid!")
+
+            res.end(content);
+        });
+
+    } else {
+        res.statusCode = 404;
+        res.end("Not Found");
+    }
+
+}
+
+var server = new http.createServer(handler);
+module.exports = server;
