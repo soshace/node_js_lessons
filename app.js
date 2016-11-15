fb6ec3dc80e1361e3077b1c0b1942d3f871c0efd
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-15 16:51:28 +0300

    domain_1-12

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var domain = require('domain');
 var serverDomain = domain.create();
 
-var server = require('./server');
-
 serverDomain.on('error', function(err) {
     console.error("Domain has caught %s", err);
 });
 
 serverDomain.run(function() {
+    var server = require('./server');
     server.listen(3000);
 });
