b97088daf3ffd612f09d72ee33e133944d062b43
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-15 15:49:05 +0300

    domain_1-9

--- domain.js
+++ domain.js
@@ -1,20 +1,24 @@
 var domain = require('domain');
-var fs = require('fs');
+var fs = require('fs'), http = require('http');
+
 var d = domain.create(), server;
 
 d.on('error', function(err) {
     console.error("Domain has caught %s", err);
 });
 
-
 d.run(function() {
 
-    setTimeout(function() {
-      fs.readFile(__filename, function() {
-          ERROR();
-      });
-        }, 1000);
-
+    server = new http.Server();
 
 });
 
+server.on('boom', function() {
+    setTimeout(function() {
+        fs.readFile(__filename, function() {
+            ERROR();
+        });
+    }, 1000);
+});
+
+server.emit('boom');
