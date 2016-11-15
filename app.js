d5a01433ce47ca4e8076301b35a740fa17689b60
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-15 17:24:13 +0300

    domain_1-13

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 var domain = require('domain');
 var serverDomain = domain.create();
 
+var server;
+
 serverDomain.on('error', function(err) {
-    console.error("Domain has caught %s", err);
+   console.error("Server error", err);
+   if (server) server.close();
+
+   setTimeout(function () {
+       process.exit(1);
+   }, 1000).unref();
 });
 
+
 serverDomain.run(function() {
-    var server = require('./server');
-    server.listen(3000);
+    var http = require('http');
+    var handler = require('./handler');
+    //var database = require ('mongodb');
+
+     server = http.createServer (function(req, res) {
+
+       var reqDomain = domain.create();
+        reqDomain.add(req);
+        reqDomain.add(res);
+
+        reqDomain.on('error', function(err) {
+          res.statusCode = 500;
+          res.end("Sorry, " + err);
+           // ...
+          serverDomain.emit('error', err);
+        });
+
+        reqDomain.run(function() {
+           handler(req, res);
+         });
+       });
+
+     server.listen(3000);
 });
--- handler.js
+++ handler.js
@@ -1,7 +1,6 @@
-var http = require('http');
 var fs = require('fs');
 
-function handler(req, res) {
+module.exports = function handler(req, res) {
     if (req.url == '/') {
 
         fs.readFile('no-such-file', function(err, content) {
@@ -16,7 +15,4 @@ function handler(req, res) {
         res.end("Not Found");
     }
 
-}
-
-var server = new http.createServer(handler);
-module.exports = server;
+};
