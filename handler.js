a1110a123d11cdf55cd63719f74d72eae89aa1f0
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-16 16:00:35 +0300

    domain_1-15

--- handler.js
+++ handler.js
@@ -5,11 +5,11 @@ var redis = require('redis').createClient();
 module.exports = function handler(req, res) {
     if (req.url == '/') {
 
-        redis.get('data', function(err, data) {
+        redis.get('data', process.domain.bind(function(err, data) {
 
-            throw new Error("redis callback"); //
+            throw new Error("redis callback");
 
-        });
+        }));
 
     } else {
         res.statusCode = 404;
