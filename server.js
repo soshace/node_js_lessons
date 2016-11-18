f3cfc9a15e88e0c4364a237085e7141e4257bf49
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-18 17:36:47 +0300

    read-params_1-4

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 // supervisor -- server.js --port=3000
 
+console.log(process.env.HOME);
+
 var http = require('http');
 
 var opts = require('optimist').argv;
 
 http.createServer(function(req, res) {
+
+    if (process.env.NODE_ENV == 'production') {
+        // optimization
+    } else if (process.env.NODE_ENV == 'development') {
+            // additional debugging output
+
+        }
 
     res.end("The server is running!");
 
