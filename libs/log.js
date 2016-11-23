9a945af2f9241d7808001c045c983522c5a9b1c2
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-23 15:07:11 +0300

    chat-foundation-1-8

--- libs/log.js
+++ libs/log.js
@@ -4,8 +4,7 @@ var ENV = process.env.NODE_ENV;
 // can be much more flexible than that O_o
 function getLogger(module) {
 
-    var path = module.filename.split('/').slice(-2).join('/');
-
+    var path = module.filename.split('\\').slice(-2).join('\\');
     return new winston.Logger({
         transports: [
             new winston.transports.Console({
