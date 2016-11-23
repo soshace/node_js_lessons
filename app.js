c2133745ad8f400d8c58062c0a927748cf7ecdba
Rastvorov Ivan <rastvorov_is@soshace.com>
2016-11-23 14:55:17 +0300

    chat-foundation-1-7

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var config = require('config');
+var log = require('libs/log')(module);
+
 
 var app = express();
-app.set('port', 3000);
+app.set('port', config.get('port'));
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  log.info('Express server listening on port ' + config.get('port'));
 });
 
 // Middleware
